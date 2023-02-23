145629848e8effa2de3b98f9568195fb7bd02267
brunocs90@gmail.com <brunocs90@gmail.com>
2023-02-23 13:27:56 -0300

    using theme and save localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { ThemeProvider } from 'styled-components';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
 import Header from './components/Header';
+import usePeristedState from './hooks/usePersistedState';
 import { GlobalStyle } from './styles/global';
+import { darkTheme } from './styles/themes/dark';
 import { lightTheme } from './styles/themes/light';
 
 export function App() {
+    const [theme, setTheme] = usePeristedState<DefaultTheme>('theme', lightTheme);
+
+    const toggleTheme = () => {
+        setTheme(theme.title === 'light' ? darkTheme : lightTheme);
+    };
+
     return (
-        <ThemeProvider theme={lightTheme}>
+        <ThemeProvider theme={theme}>
             <GlobalStyle />
-            <Header />
+            <Header toggleTheme={toggleTheme} />
         </ThemeProvider>
     );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,27 @@
+import { useContext } from 'react';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
 import { Container } from './styles';
 
-export default function Header() {
+interface Props {
+    toggleTheme(): void;
+}
+
+export default function Header({ toggleTheme }: Props) {
+    const { colors, title } = useContext(ThemeContext);
     return (
         <Container>
             <div>Hello World</div>
+            <Switch
+                onChange={toggleTheme}
+                checked={title === 'dark'}
+                checkedIcon={false}
+                uncheckedIcon={false}
+                height={10}
+                width={40}
+                handleDiameter={20}
+                onColor={colors.secundary}
+            />
         </Container>
     );
 }
