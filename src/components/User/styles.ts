c1cc4b382931f1b456085504b183de87b419a1d2
brunocs90@gmail.com <brunocs90@gmail.com>
2023-02-25 14:18:40 -0300

    user header new

--- src/components/User/styles.ts
+++ src/components/User/styles.ts
@@ -6,9 +6,7 @@ export const UserContainer = styled.header`
     display: flex;
     margin-top: 1.5rem;
     border-radius: 0.938rem;
-
     padding: 2.75rem 3rem 3rem 3rem;
-
     background-color: ${({ theme }) => theme.colors['section-background']};
 
     border: solid blue;
