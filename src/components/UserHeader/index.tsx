076a8bc5d3d3b64e8373ee4720802858501de2d1
brunocs90@gmail.com <brunocs90@gmail.com>
2023-02-26 12:22:30 -0300

    userHeader with gridColumns

--- src/components/UserHeader/index.tsx
+++ src/components/UserHeader/index.tsx
@@ -3,10 +3,23 @@ import { UserHeaderContainerBio } from './styles';
 export default function UserHeader() {
     return (
         <UserHeaderContainerBio>
-            <div id="item-1">imagem</div>
-            <div id="item-2">Usuario</div>
-            <div id="item-3">Joined</div>
-            <div id="item-4">Bio</div>
+            <div id="item-1">
+                <img className="avatar" src="https://github.com/brunocs90.png" alt="" />
+            </div>
+            <div id="item-2">
+                <div>
+                    <h2 className="user">The Octocatdsadasd</h2>
+                    <a className="login" href="">
+                        @octocat
+                    </a>
+                </div>
+                <p className="join">Joined 25 Jan 2011</p>
+            </div>
+            <div id="item-3">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.
+                </p>
+            </div>
         </UserHeaderContainerBio>
     );
 }
--- src/components/UserHeader/styles.ts
+++ src/components/UserHeader/styles.ts
@@ -1,34 +1,66 @@
 import styled from 'styled-components';
 
 export const UserHeaderContainerBio = styled.div`
-    border: 3px solid red;
+    height: 8.438rem;
     display: grid;
-    grid-template-columns: auto auto auto;
+    grid-template-columns: 7.313rem auto auto;
+    grid-template-rows: 4.063rem auto;
+    border: solid red;
 
     div {
         color: black;
-        background-color: #ccc;
-        padding: 20px;
         border: 1px solid blue;
     }
 
     #item-1 {
         grid-row-start: 1;
-        grid-row-end: 4;
+        grid-row-end: 3;
+
+        .avatar {
+            width: 7.313rem;
+            height: 7.313rem;
+            border-radius: 100px;
+        }
     }
 
     #item-2 {
         grid-column-start: 2;
-        grid-column-end: 3;
+        grid-column-end: 4;
+        margin-left: 2.25rem;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: top;
+
+        .user {
+            color: ${({ theme }) => theme.colors['text-primary']};
+            font-size: ${({ theme }) => theme.Typography.H1['font-size']};
+            font-weight: ${({ theme }) => theme.Typography.H1['font-weight']};
+            line-height: ${({ theme }) => theme.Typography.H1['line-height']};
+        }
+
+        .login {
+            padding-top: 2px;
+            color: ${({ theme }) => theme.colors.link};
+        }
+
+        .join {
+            color: ${({ theme }) => theme.colors['text-primary']};
+            font-size: ${({ theme }) => theme.Typography.Body['font-size']};
+            font-weight: ${({ theme }) => theme.Typography.Body['font-weight']};
+            line-height: ${({ theme }) => theme.Typography.Body['line-height']};
+            padding-top: 7px;
+        }
     }
 
     #item-3 {
-        grid-column-start: 3;
-        grid-column-end: 4;
-    }
-
-    #item-4 {
         grid-column-start: 2;
         grid-column-end: 4;
+        margin-left: 2.25rem;
+
+        color: ${({ theme }) => theme.colors['text-primary']};
+        font-size: ${({ theme }) => theme.Typography.Body['font-size']};
+        font-weight: ${({ theme }) => theme.Typography.Body['font-weight']};
+        line-height: ${({ theme }) => theme.Typography.Body['line-height']};
     }
 `;
